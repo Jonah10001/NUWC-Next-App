8a53db95abc5288682b81a2ceb00ae8504a3fac5
kotasuzuki3 <kota.suzuki2021@gmail.com>
2023-10-24 22:45:00 -0500

    minor decorations

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,12 +7,11 @@ export default function Contact() {
     <div>
       <Navbar />
       <div className="bg-gradient-to-b from-transparent via-white to-bg-start h-screen flex flex-col justify-center items-center">
-        <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
         <div className="contact-content-container flex items-center justify-center space-x-8">
           <img
             src="/transparentnuwclogo.png" 
             alt="Club Logo"
-            className="w-1/3" 
+            className="w-1/2" 
           />
           <div className="contact-card-container space-y-4">
             <div className="contact-card contact-card-President bg-white p-4 rounded-lg shadow-md flex">
@@ -23,7 +22,7 @@ export default function Contact() {
                   className="w-32 h-32 rounded-full mr-4"
                 />
               </div>
-              <div className="card-details" style={{ marginBottom: '20px' }}>
+              <div className="card-details">
                 <h2 className="text-xl font-semibold">President</h2>
                 <p>Name: Ben Kofman</p>
                 <p>Email: <a href="mailto:benkofman2024@u.northwestern.edu" className="text-blue-500 hover:underline">benkofman2024@u.northwestern.edu</a></p>
@@ -37,7 +36,7 @@ export default function Contact() {
                   className="w-32 h-32 rounded-full mr-4"
                 />
               </div>
-              <div className="card-details" style={{ marginBottom: '20px' }}>
+              <div className="card-details">
                 <h2 className="text-xl font-semibold">Vice President</h2>
                 <p>Name: Alex Margolis</p>
                 <p>Email: <a href="mailto:alexmargolis2024@u.northwestern.edu" className="text-blue-500 hover:underline">alexmargolis2024@u.northwestern.edu</a></p>
@@ -51,7 +50,7 @@ export default function Contact() {
                   className="w-32 h-32 rounded-full mr-4"
                 />
               </div>
-              <div className="card-details" style={{ marginBottom: '20px' }}>
+              <div className="card-details">
                 <h2 className="text-xl font-semibold">Treasurer</h2>
                 <p>Name: Lauren Escudero</p>
                 <p>Email: <a href="mailto:laurenescudero2025@u.northwestern.edu" className="text-blue-500 hover:underline">laurenescudero2025@u.northwestern.edu</a></p>
@@ -62,20 +61,28 @@ export default function Contact() {
       </div>
       <style jsx>
         {`
+          .contact-content-container {
+            max-height: 80vh; 
+            margin-bottom: 20px; 
+          }
+
           .contact-card {
             position: relative;
             overflow: hidden;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
           }
 
           .contact-card-President::before {
             content: "";
             position: absolute;
-            top: -70px;
+            top: -55px;
             right: 0;
             bottom: 0;
             left: 0;
             background-image: url('https://wallpapers.com/images/featured/chelsea-fc-logo-esvpoici90jl4fpx.jpg');
-            background-size: cover;
+            background-size: 127%;
             background-repeat: no-repeat;
             opacity: 0.2;
             z-index: 1;
@@ -91,6 +98,7 @@ export default function Contact() {
             background-image: url('https://www.thesun.co.uk/wp-content/uploads/2023/04/detailed-view-badge-manchester-united-811167421-1.jpg');
             background-size: cover;
             background-repeat: no-repeat;
+            background-position: left;
             opacity: 0.2;
             z-index: 1;
           }
@@ -105,6 +113,7 @@ export default function Contact() {
             background-image: url('https://images2.minutemediacdn.com/image/upload/c_crop,w_5156,h_2900,x_0,y_324/c_fill,w_1440,ar_16:9,f_auto,q_auto,g_auto/images/GettyImages/mmsport/90min_en_international_web/01g3xsrq61he6r6rjx4t.jpg');
             background-size: cover;
             background-repeat: no-repeat;
+            background-position: 0% 30%;
             opacity: 0.2;
             z-index: 1;
           }
