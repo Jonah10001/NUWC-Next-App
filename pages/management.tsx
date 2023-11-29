0f0c886b0a3db6d7f22b9740ddb506b0525436cd
Jonah10001 <95149794+Jonah10001@users.noreply.github.com>
2023-11-28 19:54:29 -0600

    pushing

---
+++ pages/management.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../app/globals.css';
+
+
+export default function Management() {
+  return (
+    <div className="flex justify-center items-center"> <p> Management </p> </div>
+  )
+}
